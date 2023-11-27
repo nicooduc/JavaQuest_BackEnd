d0d4681d0da8c555e6e34e9c0d79718fe4304cae
marc65 <64306072+marc65@users.noreply.github.com>
2023-11-27 09:35:43 +0100

    fix hero duplicate

--- src/main/java/com/epf/javaquest/services/FightService.java
+++ src/main/java/com/epf/javaquest/services/FightService.java
@@ -80,7 +80,8 @@ public class FightService {
             expGain = hero.updateExp(expGain);
             heroDao.save(hero);
         } else {
-            heroDao.deleteById(1L); // update 0 if multiple heroes available
+            //heroDao.deleteById(1L); // update 0 if multiple heroes available
+            heroDao.deleteAll();
             heroDao.save(Hero.createHero(hero.getName(), 1));
             expGain = 0;
         }
